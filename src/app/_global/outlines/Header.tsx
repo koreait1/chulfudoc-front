a873eca47edd8b96cd9bdd0cb38db01ac38a7a61
juyonghyeon <jyh928578@gmail.com>
2025-08-11 13:20:25 +0900

    공통소스 작업중

---
+++ src/app/_global/outlines/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => {
+  return <header>헤어 영역</header>
+}
+
+export default React.memo(Header)
---
+++ src/app/_global/styles/fontsize.ts
@@ -0,0 +1,11 @@
+/* 이거는 바꿀 필요가 없을거 같아요 */
+const fontsize = {
+  tiny: '0.65rem',
+  small: '0.85rem',
+  normal: '1rem',
+  medium: '1.25rem',
+  big: '1.5rem',
+  extra: '2rem',
+}
+
+export default fontsize
