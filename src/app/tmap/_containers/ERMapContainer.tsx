24b5da970f6e7b6c8677ed073f9fcd097e58ccef
juyonghyeon <jyh928578@gmail.com>
2025-08-22 17:26:27 +0900

    티맵 테스트 중

---
+++ src/app/tmap/_containers/ERMapContainer.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import ERMapTMapV3 from '../_components/ERMapTMapV3'
+
+export default function ShowTMap() {
+  return (
+    <div>
+      <h1>ER 위치 지도</h1>
+      <ERMapTMapV3 />
+    </div>
+  )
+}
