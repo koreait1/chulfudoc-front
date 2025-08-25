fc656077672d9e3e32df3875355feaeb6512c77b
juyonghyeon <jyh928578@gmail.com>
2025-08-25 16:34:10 +0900

    호출 제한 걸렸지만 기능들은 정상 작동 확인

---
+++ src/app/tmap/_containers/GERMapContainer.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Map from "@/app/_global/components/Map"
+
+export default function ShowMap() {
+  return (
+    <div>
+      <h1>가장 가까운 병원위치지도</h1>
+      <Map />
+    </div>
+  )
+}
