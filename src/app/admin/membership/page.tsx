2348a2fcea790a1fd3fa61761f0a49d89f91cb47
KimMarinet <100179052+KimMarinet@users.noreply.github.com>
2025-08-27 00:26:06 +0900

    ..

--- src/app/admin/membership/page.tsx
+++ src/app/admin/membership/page.tsx
@@ -1,10 +1,18 @@
 import { MainTitle } from "@/app/_global/components/TitleBox";
+import ListContainer from "./_containers/ListContainer";
+import CommonSearchType from "@/app/_global/types/CommonSearchType";
 import AdminOnlyContainer from "@/app/_global/wrappers/AdminOnlyContainer";
 
-export default async function name() {
+export default async function MemberListPage({
+    searchParams,
+}: {
+    searchParams: Promise<CommonSearchType>
+ }) {
+    const params = await searchParams
     return(
         <AdminOnlyContainer>
-            <MainTitle border={true} borderThickness={"1px"}>회원 관리</MainTitle>
+            <MainTitle border="true" borderThickness={"1px"}>회원 관리</MainTitle>
+            
         </AdminOnlyContainer>
     )
 }
