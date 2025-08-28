d4a337c4e46af82dff6fa0e33990ddd62efa7ffb
juyonghyeon <jyh928578@gmail.com>
2025-08-28 14:28:47 +0900

    에러페이지 작업, 비회원이 마이페이지 사이드바 이용하는거 수정

--- src/app/mypage/outlines/Side.tsx
+++ src/app/mypage/outlines/Side.tsx
@@ -2,10 +2,16 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import useUser from "@/app/_global/hooks/useUser";
 
 const MypageSide = styled.div``
 
 const Side = () => {
+  const { isLogin } = useUser()
+
+  if (!isLogin) {
+    return null
+  }
   return (
     <MypageSide>
       <Link href="/mypage/profile">회원정보 수정</Link>
