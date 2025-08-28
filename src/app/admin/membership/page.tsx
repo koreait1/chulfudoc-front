b3f94214bfbb8643fd38f94324fdba685eaa3bdf
jysTroy <jys_troy@naver.com>
2025-08-29 01:13:29 +0900

    어드민 회원 작업

--- src/app/admin/membership/page.tsx
+++ src/app/admin/membership/page.tsx
@@ -1,21 +1,19 @@
-import { MainTitle } from "@/app/_global/components/TitleBox";
-import ListContainer from "./_containers/ListContainer";
-import CommonSearchType from "@/app/_global/types/CommonSearchType";
-import AdminOnlyContainer from "@/app/_global/wrappers/AdminOnlyContainer";
+import ListContainer from './_containers/MemberListContainer'
+import { getMemberList } from '@/app/member/_services/actions'
+import type CommonSearchType from '@/app/_global/types/CommonSearchType'
+import AdminOnlyContainer from '@/app/_global/wrappers/AdminOnlyContainer'
+import { MainTitle } from '@/app/_global/components/TitleBox'
 
 export default async function MemberListPage({
-    searchParams,
-}: {
-    searchParams: Promise<CommonSearchType>
- }) {
-    const params = await searchParams
+  searchParams,
+}: { searchParams: Promise<CommonSearchType> }) {
+  const params = await searchParams
+  const { items, pagination } = await getMemberList(params)
 
-    return (
-      <AdminOnlyContainer>
-        <MainTitle border="true">
-          회원 관리
-        </MainTitle>
-        <ListContainer />
-      </AdminOnlyContainer>
-    )
+  return (
+    <AdminOnlyContainer>
+      <MainTitle border="true">회원 목록</MainTitle>
+      <ListContainer items={items} pagination={pagination} search={params} />
+    </AdminOnlyContainer>
+  )
 }
