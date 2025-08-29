763ac4271b153b8575b44ce4ecff43f35dd4ab67
rmftp <kyj2001kyj@naver.com>
2025-08-29 18:21:25 +0900

    임시저장

--- src/app/mypage/_components/AccountInfo.tsx
+++ src/app/mypage/_components/AccountInfo.tsx
@@ -1,48 +1,93 @@
 'use client'
+import FileImages from '@/app/_global/components/FileImages'
 import useUser from '@/app/_global/hooks/useUser'
 import color from '@/app/_global/styles/color'
 import fontSize from '@/app/_global/styles/fontsize'
 import React from 'react'
 import styled from 'styled-components'
-const { normal, big } = fontSize
-const { primary, dark } = color
-const MyBoard = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  border: 1px solid ${dark}15;
-  border-radius: 20px;
-  li {
-    width: 90%;
-    margin: 0 auto;
-    padding: 10px;
-    font-size: ${normal};
-    border: 1px soild ${dark}15;
-    &:first-child {
+import noprofile from '@/app/_global/assets/images/noprofile.png'
+import { Button } from '@/app/_global/components/Buttons'
+const { dark } = color
+const { small } = fontSize
+const MyInfo = styled.table`
+  padding: 50px 10px;
+  min-width: 320px;
+  max-width: 1150px;
+  margin: 0 auto;
+  width: 100%;
+  .profile {
+    img {
+      border-radius: 50%;
       width: 100%;
-      background: ${primary};
-      font-weight: bold;
-      font-size: ${big};
-      line-height: 1.5em;
-      border-radius: 20px 20px 0 0;
+      height: 100%;
     }
-    &:nth-child(n + 3) {
-      border-top: 1px solid ${dark};
+  }
+  .info {
+    th,
+    td {
+      span {
+        display: block;
+        font-size: ${small};
+        color: ${dark};
+      }
     }
-    &.noCon {
-      font-style: italic;
-      text-align: center;
+  }
+  th,
+  td {
+    padding: 10px 15px;
+    vertical-align: middle;
+  }
+  th {
+    text-align: right;
+    width: 150px;
+  }
+  tr:last-child {
+    td {
+      padding: 10px 15px;
+      text-align: right;
     }
   }
 `
-const WrittenList = ({ children }) => {
+const UserInfo = () => {
   const { loggedMember } = useUser()
   return (
-    <MyBoard>
-      <li>{loggedMember.name}님이 작성하신 글</li>
-      {children}
-    </MyBoard>
+    <MyInfo>
+      <tbody>
+        <tr className="profile">
+          <th>
+            <FileImages
+              items={loggedMember.profileImage}
+              viewOnly={true}
+              fallbackImage={noprofile}
+              viewOrgImage={false}
+              width={40}
+              height={40}
+            />
+          </th>
+          <td>
+            <span>{loggedMember.name} 님</span>
+          </td>
+        </tr>
+        <tr className="info">
+          <th>
+            <span>이메일</span>
+            <span>휴대전화 번호</span>
+            <span>회원가입 일자</span>
+          </th>
+          <td>
+            <span>{loggedMember.email}</span>
+            <span>{loggedMember.mobile}</span>
+            <span>{loggedMember.createdAt}</span>
+          </td>
+        </tr>
+        <tr>
+          <a href="/mypage/profile">
+            <Button>회원정보 수정</Button>
+          </a>
+        </tr>
+      </tbody>
+    </MyInfo>
   )
 }
 
-export default React.memo(WrittenList)
+export default React.memo(UserInfo)
