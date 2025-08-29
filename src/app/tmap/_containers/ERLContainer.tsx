5373d93e90016bdc85253c4f55fc01d93e46153e
juyonghyeon <jyh928578@gmail.com>
2025-08-29 17:10:59 +0900

    티맵 에러 처리 완료

--- src/app/tmap/_containers/ERLContainer.tsx
+++ src/app/tmap/_containers/ERLContainer.tsx
@@ -1,11 +1,16 @@
 'use client'
 
+import { useState } from 'react'
 import NearERInfo from '../_components/NearERInfo'
+import NearERMap from '../_components/NearERMap'
 
 export default function ERLContainer() {
+  const [blocked, setBlocked] = useState(false) // 호출제한 상태
+
   return (
     <div>
-      <NearERInfo />
+      <NearERMap onBlocked={setBlocked} />
+      {!blocked && <NearERInfo />}
     </div>
   )
 }
--- src/app/tmap/page.tsx
+++ src/app/tmap/page.tsx
@@ -4,8 +4,6 @@ import ERLContainer from './_containers/ERLContainer'
 export default function TMapPage() {
   return (
     <>
-      <ERMapContiner />
-      {/*<ERLContainer />*/} {/* API 호출을 너무 잡아먹어서 주석 처리 */}
       <ERLContainer />
     </>
   )
