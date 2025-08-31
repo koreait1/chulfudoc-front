336fe7cfc9ea9c0f859685682db2451b0a46e1fb
rmfTp <kyj2001kyj@naver.com>
2025-08-31 15:00:44 +0900

    안쓰는 참조 삭제

--- src/app/mypage/profile/page.tsx
+++ src/app/mypage/profile/page.tsx
@@ -1,4 +1,3 @@
-import ContentBox from '@/app/_global/components/ContentBox'
 import UserOnlyContainer from '@/app/_global/wrappers/UserOnlyContainer'
 import ProfileContainer from '../_containers/ProfileContainer'
 import { MainTitle } from '@/app/_global/components/TitleBox'
